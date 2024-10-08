2607aaa5550581095f1481ba308712162365b893
Jessua Solis <147453642+Jessuapapu@users.noreply.github.com>
2024-10-07 23:36:51 -0600

    Termino el lab3 de AED

--- Lab2 - AED/Banco.xaml.cs
+++ Lab2 - AED/Banco.xaml.cs
@@ -21,9 +21,11 @@ namespace Lab2___AED
     /// </summary>
     public partial class Banco : UserControl
     {
+       
         public Banco()
         {
             InitializeComponent();
+            rellenar();
         }
 
         // Este evento se dispara cuando el UserControl ha cargado
@@ -41,6 +43,14 @@ namespace Lab2___AED
                 // Ajustamos la posición del Popup
                 popupTurnoC.HorizontalOffset = rightPosition;
                 popupTurnoC.VerticalOffset = topPosition;
+
+                // Calculamos la posición para que el Popup popupTurnoM aparezca justo debajo de la ventana
+                double leftPositionM = (mainWindow.Width / 2 - popupTurnoM.ActualWidth / 2) - 250;
+                double bottomPosition = mainWindow.Height + 40; // Ajuste de 20 píxeles debajo de la ventana
+
+                // Ajustamos la posición del Popup popupTurnoM
+                popupTurnoM.HorizontalOffset = leftPositionM;
+                popupTurnoM.VerticalOffset = bottomPosition;
             }
         }
         // cerrar popup
@@ -56,7 +66,102 @@ namespace Lab2___AED
 
         private void cerrarMesa_Click(object sender, RoutedEventArgs e)
         {
-
+            popupTurnoM.IsOpen = false;
         }
+
+        private void MostrarMesa(object sender, RoutedEventArgs e)
+        {
+            popupTurnoM.IsOpen = true;
+        }
+
+        private void Agregar_Click(object sender, RoutedEventArgs e)
+        {
+                Encolar();
+        }
+
+        private void SiguienteCaja_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DesencolarCaja();
+            }
+            catch
+            {
+                MessageBox.Show("No hay personas en la cola");
+            }
+        }
+
+        private void SiguienteMesa_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            { 
+                DesencolarMesa();
+            }
+            catch
+            {
+                MessageBox.Show("No hay personas en la cola");
+            }
+        }
+
+
+        // Declaraciones de botones
+
+        Queue<Personas> cola = new Queue<Personas>();
+        Queue<Personas> mesa = new Queue<Personas>();
+
+        void rellenar()
+        {
+            cbServicios.Items.Add("Deposito");
+            cbServicios.Items.Add("Retiro");
+            cbServicios.Items.Add("Consulta");
+            cbServicios.Items.Add("Pago de Servicios");
+            cbServicios.Items.Add("Apertura de Cuenta");
+
+            cbTipo.Items.Add("caja");
+            cbTipo.Items.Add("Servicio Bancarios");
+        }
+
+        public void Encolar()
+        {
+            if(txtNombre.Text == "" || cbServicios.Text == "" || cbTipo.Text == "")
+            {
+                MessageBox.Show("Ingrese todos los datos");
+                return;
+            }
+            Personas persona = new Personas(txtNombre.Text, cbServicios.Text);
+
+            if (cbTipo.Text == "caja")
+            {
+                cola.Enqueue(persona);
+            }
+            else
+            {
+                mesa.Enqueue(persona);
+            }
+            MostrarColas();
+        }
+
+        public void MostrarColas()
+        {
+            var Personas = cola.Take(4);
+            dgCaja.ItemsSource = Personas;
+            txtSiguienteAPasarC.Text = cola.Peek().Nombre;
+            var PersonasMesa = mesa.Take(4);
+            txtSiguienteAPasarM.Text = mesa.Peek().Nombre;
+            dgMesa.ItemsSource = PersonasMesa;
+        }
+
+        public void DesencolarMesa()
+        {
+            cola.Dequeue();
+            MostrarColas();
+        }
+
+        public void DesencolarCaja()
+        {
+            mesa.Dequeue();
+            MostrarColas();
+        }
+
     }
 }
---
+++ Lab2 - AED/Personas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2___AED
+{
+    public class Personas 
+    {
+
+        public string Nombre { get; set; }
+        public string Consulta { get; set; }
+
+        public Personas(string Nombre,string Consulta)
+        {
+            this.Nombre = Nombre;
+            this.Consulta = Consulta;
+        }
+    
+    }
+}
